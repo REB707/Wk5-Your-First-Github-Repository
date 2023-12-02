8d2fcf88a082d75897d7d8f7b5d3464859afeaa8
REB707 <osocold707@gmail.com>
2023-12-02 15:25:11 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Put down your name
+my_name = "Roy Bowden"
+# Put down the course name you're working on
+course_name = "Python Programming: 2023FA-CIS-024C-102"
+# Current month (in integer please!)
+curr_month = 9 # change this to ..?
+
+print(f"Hello {my_name}! The course ends on Dec, which is {12-curr_month} months from now.")
+print("Hope you have a great time learing this course")
+
+today = 71.5
+yesterday = 73.5
+# print out the average temperuatre of today and yesterday
+# hint: avg = (a+b) / 2
+avg_temp = (today+yesterday)/2 # fill in here!
+print(f"average temperature is {avg_temp}")
